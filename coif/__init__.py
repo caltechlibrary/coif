54cd8a81e7b2e4ae7ac08c41b940b7f00f24556b
mhucka <mhucka@caltech.edu>
2021-09-10 13:02:49 -0700

    Add __init__.py

---
+++ coif/__init__.py
@@ -0,0 +1,36 @@
+'''
+Coif: Cover image finder
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2021 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+# Package metadata ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#  ╭────────────────────── Notice ── Notice ── Notice ─────────────────────╮
+#  |    The following values are automatically updated at every release    |
+#  |    by the Makefile. Manual changes to these values will be lost.      |
+#  ╰────────────────────── Notice ── Notice ── Notice ─────────────────────╯
+
+__version__     = '0.0.1'
+__description__ = 'Cover image finder: find a cover image given a book identifier'
+__url__         = 'https://github.com/caltechlibrary/coif'
+__author__      = 'Michael Hucka'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD 3-clause'
+
+
+# Exports.
+# .............................................................................
+
+from .exceptions import ServiceError, NetworkError
+from .coif       import cover_image
